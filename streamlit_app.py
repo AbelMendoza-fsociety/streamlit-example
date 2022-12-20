a88dec0c0157e88604e5452ae373b560b4c1d0a5
AbelMendoza-fsociety <120528976+AbelMendoza-fsociety@users.noreply.github.com>
2022-12-19 20:09:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,13 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 """
-# Welcome to Streamlit!
+# Universidad Nacional de SAn Agustin de Arequipa!
+## Escuela Profesional de Ingeniería de Telecomunicaciones!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
